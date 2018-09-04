750aade1a55d9960ca32aee2004246e362b0036e
APCS3rd <APCS3rd@MHS-Math2-22>
2018-09-04 09:45:38 -0700

    hello

---
+++ First Coding Project/src/Hello_World.java
@@ -0,0 +1,15 @@
+/**
+ * Coding Project
+ * @author Jonathan Sui 
+ * @version 8/17/18
+ * this displays the string
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Jonathan"); 
+        System.out.println("Say Something");
+        System.out.println("Computers are interesting");
+    }
+}
---
+++ Pikachu 3rd period/src/Pikachu.java
@@ -0,0 +1,12 @@
+/*This code will print out Pikachu	
+ *@author Jonathan Sui
+ *@version 8/31/18
+ */
+public class Pikachu {
+
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("				")
+	}
+
+}
